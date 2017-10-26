12bad282396fbac4f2797454686c20b8ddaf086a
Gopikrishna <a.gopikrishna01@gmail.com>
2017-10-26 11:21:48 +0530

    Create 001_triangle_pattern.c

---
+++ 002_C_Patterns/001_triangle_pattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int cnt = 1;
+	int num = 5;
+	printf("Enter number of rows of the pyramid: ");
+	scanf("%d",&num);
+	int i;
+	int j;
+	for (i = 1;i<num+1;i++)
+	{
+		for (j = 0; j < i ; j++ )
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
