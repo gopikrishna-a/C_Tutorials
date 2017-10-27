ebb8c578f864ded30f9439139e144167be355cb6
Gopikrishna <a.gopikrishna01@gmail.com>
2017-10-27 11:21:26 +0530

    Create 014_functions.c

---
+++ 001_C_Language_Tutorials/014_functions.c
@@ -0,0 +1,48 @@
+//Comparing Two numbers using functions!!
+
+#include <stdio.h>
+
+int compare_three_nums(int a, int b); //function definition to compare the values
+
+int main()
+
+{
+	int a;
+	int b;
+	int res;
+	printf("Enter value of a: ");
+	scanf("%d", &a);
+	printf("Enter value of b: ");
+	scanf("%d", &b);
+
+
+	compare_three_nums(a, b);
+	printf("Comparision completed!!!\n");
+	return 0;
+
+}
+
+
+int compare_three_nums(int a, int b) //function declaration
+
+{
+
+	if (a > b)
+
+	{
+	    printf("a is bigger!!\n");
+	}
+	    
+	else if (b > a)
+	{
+	    printf("b is bigger!!\n");
+	}
+	else
+	{
+		printf("a and b are equal!!\n");
+	} 
+	    
+return 0;
+
+}
+
